127914981fb09115bffb3a01f6ae9eba9ec8980c
Alex Njagi <1992anjagi@gmail.com>
2023-03-13 14:45:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1;
+    num1=31;
+let num2;
+    num2=2;
+const multiply = num1*num2;
+
+let random;
+    random=Math.floor(Math.random()*5);
+
+let num3;
+    num3=68;
+let num4;
+    num4=8;
+const mod = num3 % num4;
+
+const max = Math.max(5,10,15,20);
